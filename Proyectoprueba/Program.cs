2b48f0c2e887653c5e50f7535a0aa50cd3875227
Facundo-Lesteyme <facuuless@gmail.com>
2023-03-21 12:04:29 -0300

    Primer commit prueba

---
+++ Proyectoprueba/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyectoprueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wep");
+            Console.WriteLine("5+5=" + (5 + 5)); 
+            Console.Read();
+        }
+    }
+}
